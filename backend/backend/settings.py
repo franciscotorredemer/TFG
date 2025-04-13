fc3a56cfe7596806c79286c13e09285d8d4d010d
francisco <francisco.torredemer@estudiantat.upc.edu>
2025-04-13 14:21:57 +0200

    añadido el deploy a los allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-90n!j9=&aioz%etwhs9dfr6xc+fecahmwfmu5^0i-@_4aba=8^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["tfg-production-e77a.up.railway.app"]
 
 
 # Application definition
