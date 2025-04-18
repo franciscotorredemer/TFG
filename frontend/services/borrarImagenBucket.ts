c3d74f802f7b4983e529a70cb20ea42334808f64
francisco <francisco.torredemer@estudiantat.upc.edu>
2025-04-18 18:47:00 +0200

    correccion bug

--- frontend/services/borrarImagenBucket.ts
+++ frontend/services/borrarImagenBucket.ts
@@ -1,14 +1,13 @@
-import { bucket } from "./bucket"
+import { bucket } from './bucket'
 
 export const borrarImagenPerfil = async (nombreArchivo: string) => {
-  try {
-    const { error } = await bucket.storage
-      .from("fotosperfil")
-      .remove([`perfil/${nombreArchivo}`])
+  const { error } = await bucket.storage
+    .from("fotosperfil")
+    .remove([`perfil/${nombreArchivo}`])
 
-    if (error) throw error
-    console.log("✅ Imagen borrada del bucket:", nombreArchivo)
-  } catch (error) {
-    console.error("❌ Error al borrar imagen del bucket:", error)
+  if (error) {
+    console.error(" Error al borrar imagen:", error)
+  } else {
+    console.log(" Imagen borrada del bucket:", nombreArchivo)
   }
 }
